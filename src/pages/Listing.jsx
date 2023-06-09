eb7c21c0652872979581a83fdb210712a12bb28b
Divesh Thapa <dthapa007@gmail.com>
2023-06-09 07:49:03 +0545

    search bar palced ta center

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import "./listing.css"
 
 const Listing = () => {
   return (
-    <div>Listing</div>
+    <div id ='Listing'>
+       <div id='listing-search-bar' className='text-center'>
+        <input type='text' placeholder='Search any Listings'/>
+        <i class="fa fa-search" aria-hidden="true"></i>
+    </div>
+    </div>
   )
 }
 
