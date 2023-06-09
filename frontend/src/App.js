3a3b287bac858c65482ea7341b67241c8f00539d
Divesh Thapa <dthapa007@gmail.com>
2023-06-09 08:47:02 +0545

    bcakend folder addred with models, controller, routes

---
+++ frontend/src/App.js
@@ -0,0 +1,32 @@
+import { BrowserRouter, Routes,Route } from 'react-router-dom';
+import './App.css';
+import { Navbar } from './component/Navbar';
+import Homepage from './pages/Homepage';
+import Details from './component/Home/Details';
+import Login from './component/Home/Login';
+import RegisterPage from './component/Home/RegisterPage';
+import Profilepage from './component/Home/Profilepage';
+import Listing from './pages/Listing';
+import Bookings from './pages/Bookings';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<Homepage />}/>
+      <Route path='/acc' element={<Details />}/>
+
+      <Route path='/login' element={<Login />}/>
+      <Route path='/register' element={<RegisterPage/>}/>
+
+      <Route path='/profilepage' element={<Profilepage/>}/>
+      <Route path='/listing' element={<Listing/>}/>
+      <Route path='/bookings' element={<Bookings/>}/>
+    </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
