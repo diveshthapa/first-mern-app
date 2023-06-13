e5781872d04d35aed8e743c3a88cd142f1b12c51
Divesh Thapa <dthapa007@gmail.com>
2023-06-13 09:08:34 +0545

    script added and env created

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,16 @@
 const express = required("express")
 const app=express()
+import("dotenv").config;
+
+const mongoose = require("mongoose")
+mongoose.connect("mongodb://localhost:27017/my-react-app").then(()=>{
+    console.log("Database Connected successfully")
+}).catch((err)=>{
+    console.log(err.message)
+})
 
 
-
-
-app.listen(5000,()=>{
+app.listen(process.env.PORT,()=>{
 console.log("Server has started in port 5000")
 
 })
