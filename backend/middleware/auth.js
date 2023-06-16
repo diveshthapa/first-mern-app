11dedceb7e108a55405124e5ea9c132103acaa4d
Divesh Thapa <dthapa007@gmail.com>
2023-06-16 08:41:16 +0545

    auth function created

---
+++ backend/middleware/auth.js
@@ -0,0 +1,41 @@
+const jwt = require("jsonwebtoken")
+const User = require("../Models/UserModel")
+
+
+exports.isAuthenticated = async (req, res, next) => {
+    try {
+        const token = req.cookies["token"];
+        if (!token) {
+            return res.status(403).json({
+                success: false,
+                message: "Please Login To Continue",
+            });
+        }
+        const decodedData = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findById(decodedData.id);
+        req.user = user;
+        next();
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+}
+
+
+exports.authorizedRole = (...roles) => {
+    return (req, res, next) => {
+        const authorized = roles.includes(req.user.role);
+        if (authorized) {
+            next();
+        } else {
+            return res.status(403).json({
+                success: false,
+                message: "Access denied",
+            });
+        }
+    }
+}
