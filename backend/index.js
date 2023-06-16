6ca3b938acec0dcb750519783d86dfe2e61c6f76
Divesh Thapa <dthapa007@gmail.com>
2023-06-16 07:39:57 +0545

    route added

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,14 @@
 const express = require("express")
 const app = express()
+const morgan = require("morgan")
 require("dotenv").config()
 require("./database/connection")
 
 app.use(express.json())
+app.use(morgan("dev"))
 
 const userRoute = require("./routes/UserRoute")
-app.use("api/user", userRoute)
+app.use("/api/user", userRoute)
 
 
 app.listen(process.env.PORT, () => {
