9ba041a9f4e3221f40adcc8cd6ff9d50b1882869
Divesh Thapa <dthapa007@gmail.com>
2023-06-21 08:53:27 +0545

    model created

---
+++ backend/Models/HotelModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose")
+
+const hotelSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        minLemght:3
+    },
+    City:{
+        type:String,
+        required:true,
+        minLemght:5
+    },
+    logo:{
+        type:String,
+        default:"https://logowik.com/hyatt-regency-hotels-logo-vector-svg-pdf-ai-eps-cdr-free-download-18072.html",
+        
+    }
+})
