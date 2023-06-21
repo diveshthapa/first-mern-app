3da520df6baa1f62632d9c00b0b1837f405299db
Divesh Thapa <dthapa007@gmail.com>
2023-06-21 09:21:36 +0545

    update

---
+++ backend/controller/HotelController.js
@@ -0,0 +1,36 @@
+const Hotel = require("../models/HotelModel")
+
+module.exports.addHotel = async (req, res) => {
+
+    try {
+        const { name, city, country } = req.body;
+        const usedHotelName = await Hotel.findOne({ name })
+        if (usedHotelName) {
+            return res.json({
+                "success": false,
+                "msg": " Hotel with this name already exists"
+            })
+        }
+
+        const newHotel = new Hotel({ name, city, country })
+        newHotel.save().then((resutl) => {
+            return res.json({
+                "success": true,
+                resutl
+            })
+        })
+            .catch((err) => {
+            return res.json({
+                "success": false,
+                "msg": error.message
+            })
+        })
+
+
+    } catch (error) {
+        return res.json({
+            "success": false,
+            "msg": error.message
+        })
+    }
+}
