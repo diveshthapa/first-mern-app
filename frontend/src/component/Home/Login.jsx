1417a337e9afd78889b9a5bb920497d10ec97a2a
Divesh Thapa <dthapa007@gmail.com>
2023-06-26 14:42:42 +0545

    page updated

--- frontend/src/component/Home/Login.jsx
+++ frontend/src/component/Home/Login.jsx
@@ -1,37 +1,37 @@
-import React, { useEffect, useState } from 'react'
-import "./login.css";
-import { Toast } from 'react-bootstrap';
+import { useState } from "react";
+import "./reg.css";
+import { toast } from "react-toastify";
+import { useDispatch } from "react-redux";
+import { login } from "../Action/userAction";
 
-const Login = () => {
+
+
+
+const LoginPage = () => {
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
 
+    const dispatch = useDispatch();
+
     const handleLogin = (e) => {
-        e.preventDefult();
+        e.preventDefault();
         if (username == "") {
-            return Toast.warn("Username can not be empyt")
-
+            return toast.warn("Username Can not be Empty")
         }
-        if (username == "") {
-            return Toast.warn("Username can not be empyt")
-
+        if (password == "") {
+            return toast.warn("Password Can not be Empty")
         }
-        dispatchlLogin(username, password)
+        dispatch(login(username, password))
     }
 
-
-
-
-
     return (
-        <div className="loginContainer">
+        <div className="registerPageContainer">
             <div className="form-container p-4">
                 <div className="text-cn text-center">
                     <h3>Login</h3>
                     <p>Please Fill Out This Form</p>
                 </div>
                 <form >
-
                     <div className="inp-container">
                         <input type="text" name='username' id="username" placeholder='Username' required onChange={(e) => setUsername(e.target.value)} />
                     </div>
@@ -39,7 +39,7 @@ const Login = () => {
                         <input type="password" name='password' id="password" placeholder='Password' required onChange={(e) => setPassword(e.target.value)} />
                     </div>
                     <div className="btn-container text-center">
-                        <button type='submit' className='btn btn-warning text-white' onClick={(e) => handleLogin(e)} > Login</button>
+                        <button type='submit' className='btn btn-warning text-white' onClick={(e) => handleLogin(e)}> Login</button>
                     </div>
                 </form>
             </div>
@@ -47,4 +47,4 @@ const Login = () => {
     )
 }
 
-export default Login
+export default LoginPage
