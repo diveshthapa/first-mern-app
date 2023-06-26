042ef95aaf325e0098dec604137a3cabadf248e7
Divesh Thapa <dthapa007@gmail.com>
2023-06-26 14:48:43 +0545

    page added and upated

--- frontend/src/Action/userAction.js
+++ frontend/src/Action/userAction.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS } from "../Constants/userConstants"
+import { LOAD_USER_FAIL, LOAD_USER_REQUEST, LOAD_USER_SUCCESS, REGISTER_USER_FAIL, REGISTER_USER_REQUEST, REGISTER_USER_SUCCESS, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS } from "../Constants/userConstants"
 import { toast } from "react-toastify"
 
 const config = {
@@ -22,5 +22,51 @@ export const login = (username, password) => async (dispatch) => {
         }
     } catch (error) {
         toast.error(error.message)
+    }
+}
+
+export const register = (reg_data) => async (dispatch) => {
+    try {
+        dispatch({ type: REGISTER_USER_REQUEST });
+        const { data } = await axios.post(
+            "/api/user/register",
+            reg_data,
+            config
+        );
+        dispatch({ type: REGISTER_USER_SUCCESS, payload: data });
+        console.log(data)
+        if (!data.success) {
+            toast.error(data.msg)
+        }
+        else {
+            toast.success(data.msg)
+        }
+    } catch (error) {
+        console.log(error)
+        dispatch({
+            type: REGISTER_USER_FAIL,
+            payload: error.response.data.message,
+        });
+    }
+};
+
+
+export const loadUser = () => async (dispatch) => {
+    try {
+        dispatch({ type: LOAD_USER_REQUEST });
+        const { data } = await axios.get(
+            "/api/user/me",
+            config
+        );
+        dispatch({ type: LOAD_USER_SUCCESS, payload: data });
+        console.log(data)
+        if (!data.status) {
+            toast.error(data.msg)
+        }
+    } catch (error) {
+        dispatch({
+            type: LOAD_USER_FAIL,
+            payload: error.response.data.message,
+        });
     }
 }
--- frontend/src/Reducer/userReducer.js
+++ frontend/src/Reducer/userReducer.js
@@ -1,8 +1,10 @@
-import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS } from "../Constants/userConstants"
+import { LOAD_USER_FAIL, LOAD_USER_REQUEST, REGISTER_USER_FAIL, REGISTER_USER_REQUEST, REGISTER_USER_SUCCESS, USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS } from "../Constants/userConstants"
 
 export const userReducer = (state = { user: {} }, action) => {
     switch (action.type) {
         case USER_LOGIN_REQUEST:
+        case LOAD_USER_REQUEST:
+        case REGISTER_USER_REQUEST:
             return {
                 loading: true,
                 isAuthenticated: false
@@ -16,10 +18,17 @@ export const userReducer = (state = { user: {} }, action) => {
             }
 
         case USER_LOGIN_FAIL:
+        case LOAD_USER_FAIL:
+        case REGISTER_USER_FAIL:
             return {
                 loading: false,
                 isAuthenticated: false,
                 user: null
+            }
+
+        case REGISTER_USER_SUCCESS:
+            return {
+                loading: false,
             }
 
 
