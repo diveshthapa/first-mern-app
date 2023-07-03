a4abb63b86fc31d2ac93208268112cecc9301201
Divesh Thapa <dthapa007@gmail.com>
2023-07-03 20:15:05 +0545

    state manage

--- frontend/src/Reducer/userReducer.js
+++ frontend/src/Reducer/userReducer.js
@@ -1,4 +1,4 @@
-import { LOAD_USER_FAIL, LOAD_USER_REQUEST, LOAD_USER_SUCCESS, REGISTER_USER_FAIL, REGISTER_USER_REQUEST, REGISTER_USER_SUCCESS, USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS } from "../Constants/userConstants"
+import { LOAD_USER_FAIL, LOAD_USER_REQUEST, LOAD_USER_SUCCESS, LOGOUT_USER_SUCCESS, REGISTER_USER_FAIL, REGISTER_USER_REQUEST, REGISTER_USER_SUCCESS, USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS } from "../Constants/userConstants"
 
 export const userReducer = (state = { user: {} }, action) => {
     switch (action.type) {
@@ -30,6 +30,12 @@ export const userReducer = (state = { user: {} }, action) => {
         case REGISTER_USER_SUCCESS:
             return {
                 loading: false,
+            }
+
+        case LOGOUT_USER_SUCCESS:
+            return {
+                user: null,
+                isAuthenticated: false
             }
 
 
