c781f16a8ce4bf0483b320d82bf99d290db1746f
Divesh Thapa <dthapa007@gmail.com>
2023-07-03 20:01:17 +0545

    updates

--- frontend/src/component/Home/HomeCard.jsx
+++ frontend/src/component/Home/HomeCard.jsx
@@ -1,20 +1,18 @@
-import React from "react"
-import { Link } from "react-router-dom"
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 const HomeCard = ({ home }) => {
     return (
-        <div className='homeCard my-4'>
-            <div className='house-image-container'>
-                <img src={home.image} alt='' />
+        <div className="homeCard" style={{ marginBottom: "8rem" }}>
+            <div className="house-image-container">
+                <img src={home.image} alt="" />
             </div>
-            <div className='house-text-container'>
-                <h4>{home.city},{home.country}</h4>
+            <div className="house-text-container">
+                <h4>{home.name}</h4>
                 <p>Rs. {home.price}/Night</p>
             </div>
-            <div>
-                <div className='house-btn-container text-center'>
-                    <Link className='btn btn-warning'to ='/Home'>Learn More</Link>
-                </div>
+            <div className="house-btn-container text-center">
+                <Link to={`/acc/${home._id}`} className='btn btn-warning'>Learn More</Link>
             </div>
         </div>
     )
