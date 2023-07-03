a0196d1af1500647a094191c5ca546c7a9b32727
Divesh Thapa <dthapa007@gmail.com>
2023-07-03 20:20:41 +0545

    remodel

--- frontend/src/component/Home/Featured.jsx
+++ frontend/src/component/Home/Featured.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import HomeCard from './HomeCard'
-import './featured.css'
-import axios from 'axios'
+import "./featured.css"
+import axios from "axios"
 import { config } from '../../Constants/const'
 
 
@@ -15,35 +15,33 @@ const Featured = () => {
     }, [])
 
     const loadFeatured = async () => {
-        const { date } = await axios.get("/api/acc/all", config)
-        setFeat(data.data.slice())
+        const { data } = await axios.get("/api/acc/all", config)
+        setFeat(data.data.slice(4))
         setLoading(false)
     }
 
 
     return (
-        <div id='featured-section' className='my-5'>
-            <div className='container'>
-                <h3> Featured Houses</h3>
+        <div id="featured-section" className='my-5'>
+            <div className="container">
+                <h3>Featured Houses</h3>
 
                 {
                     loading ? (
                         <div className='d-flex justify-content-center align-items-center'>
-                            <img src="https://uxplanet.org/using-loading-animation-on-websites-and-apps-examples-and-snippets-to-use-cab0097be9f1">
-                            </img>
-                            <>Loading</>
-
-                            ) : (
-                            <div id='houses' className='d-flex justify-content-between'>
-                                {feat.map((i) => {
-                                    return <HomeCard home={i} />
-                                })}
-
-
-                            </div>
-))
+                            <img src="https://miro.medium.com/v2/resize:fit:1400/1*CsJ05WEGfunYMLGfsT2sXA.gif" alt="" />
                         </div>
+                    ) : (
+                        <div id="houses" className='d-flex justify-content-between mb-3'>
+                            {feat.map((i) => {
+                                return <HomeCard home={i} />
+                            })}
+                        </div>
+                    )
+                }
+            </div>
         </div>
     )
 }
-            export default Featured
+
+export default Featured
