7661dbf81c2ff7c30d193b42acb81059b2be0554
Divesh Thapa <dthapa007@gmail.com>
2023-07-04 08:09:32 +0545

    recode

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,50 +1,83 @@
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Navbar from './components/Navbar';
-import Homepage from './pages/Homepage';
-import Details from './components/Home/Details';
-import RegisterPage from './pages/Register';
-import LoginPage from './pages/Login';
-import ProfilePage from './pages/Profilepage';
-import Listings from './pages/Listings';
-import Bookings from './pages/Bookings';
-import { ToastContainer } from 'react-toastify';
-import "react-toastify/dist/ReactToastify.css"
-import { useEffect } from 'react';
-
-import { useSelector } from 'react-redux';
-import { loadUser } from './Action/userAction';
-import store from './Store';
-
-function App() {
+import React, { useEffect, useState } from 'react'
+import "./listing.css"
+import HomeCard from '../components/Home/HomeCard'
+import image1 from "../images/1.png"
+import image2 from "../images/2.png"
+import image3 from "../images/3.png"
+import image4 from "../images/4.png"
+import { Spinner } from 'react-bootstrap'
+import axios from 'axios'
+import { config } from '../Constants/const'
 
 
-  const { isAuthenticated, user, loading } = useSelector((state) => state.user)
-  useEffect(() => {
-    store.dispatch(loadUser())
-  }, [])
+const houses = [
+    {
+        id: 1,
+        country: "Nepal",
+        city: "Kathmandu",
+        price: 20000,
+        image: image1
+    },
+    {
+        id: 2,
+        country: "Nepal",
+        city: "Kathmandu",
+        price: 20000,
+        image: image2
+    },
+    {
+        id: 3,
+        country: "Nepal",
+        city: "Kathmandu",
+        price: 20000,
+        image: image3
+    },
+    {
+        id: 4,
+        country: "Nepal",
+        city: "Kathmandu",
+        price: 20000,
+        image: image4
+    },
+]
+
+const Listings = () => {
+    const [properties, setProperties] = useState("")
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        getProperties()
+    }, [])
+
+    const getProperties = async () => {
+        const { data } = await axios.get("/api/acc/all", config)
+        setProperties(data.data)
+        setLoading(false)
+    }
+    return (
+        <div id="listingpage" className='my-5'>
+            <div id="listing-search-bar" className='text-center'>
+                <input type="text" placeholder='Search any listings' />
+                <i className="fa-solid fa-magnifying-glass"></i>
+            </div>
 
 
+            {
+                loading ? (
+                    <div className='d-flex justify-content-center align-items-center' >
+                        <img src="https://miro.medium.com/v2/resize:fit:1400/1*CsJ05WEGfunYMLGfsT2sXA.gif" alt="" />
+                    </div >
+                ) : (
+                    <div id="houses" className='d-flex justify-content-between container my-5 flex-wrap'>
+                        {properties.map((i) => {
+                            return <HomeCard home={i} />
+                        })}
 
-  return (
-    <BrowserRouter>
-      <Navbar isAuthenticated={isAuthenticated} user={user} />
-      <Routes>
-        <Route path='/' element={<Homepage />} />
-        <Route path='/acc/:id' element={<Details />} />
-
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/register' element={<RegisterPage />} />
-
-        <Route path='/profile' element={<ProfilePage />} />
-        <Route path='/listings' element={<Listings />} />
-
-        <Route path='/bookings' element={<Bookings />} />
-      </Routes>
-      <ToastContainer autoClose={500} />
-    </BrowserRouter>
-  );
+                    </div>
+                )
+            }
+        </div>
+    )
 }
 
-export default App;
+export default Listings
