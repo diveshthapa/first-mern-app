0f939fba369b8806d3f6e320479f5078011a78fe
Divesh Thapa <dthapa007@gmail.com>
2023-07-05 05:45:17 +0545

    modified

--- backend/routes/AccomodationRoute.js
+++ backend/routes/AccomodationRoute.js
@@ -1,12 +1,11 @@
-const { addAccomodation, getAllAccomodations, getAccomodation, updateAccomodation } = require("../controller/AccomodationController")
+const { addAccomodation, getAllAccomodations, getAccomodation, updateAccomodation } = required("../controller/AccomodationController")
 const { isAuthenticated, authorizedRole } = require("../middleware/auth")
 
 const router = require("express").Router()
 
-router.post("/acc",isAuthenticated, addAccomodation)
+router.post("/add", isAuthenticated, addAccomodation)
 router.get("/all", getAllAccomodations)
-router.get("/id", getAccomodation)
-router.patch("/",isAuthenticated, updateAccomodation)
-router.patch("/id",isAuthenticated, updateAccomodation)
+router.get("/:id", allBoth, getAccomodation)
+router.patch("/:id", isAuthenticated, updateAccomodation)
 
 module.exports = router
