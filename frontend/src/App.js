3fc7ba048c57cd844f96f86e9c2d61643e77f611
Divesh Thapa <dthapa007@gmail.com>
2023-07-05 15:10:49 +0545

    route added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,21 @@
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './component/Navbar'
 import Homepage from './pages/Homepage';
-import Details from './components/Home/Details';
-import RegisterPage from './pages/Register';
-import LoginPage from './pages/Login';
-import ProfilePage from './pages/Profilepage';
+import Details from './component/Home/Details';
+import RegisterPage from './component/Home/RegisterPage';
+import LoginPage from './component/Home/Login';
+import ProfilePage from './component/Home/ProfilePage';
 import Listings from './pages/Listings';
 import Bookings from './pages/Bookings';
 import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css"
 import { useEffect } from 'react';
-
 import { useSelector } from 'react-redux';
 import { loadUser } from './Action/userAction';
 import store from './Store';
+import User from './component/Admin/User';
 
 function App() {
 
@@ -41,6 +41,8 @@ function App() {
         <Route path='/listings' element={<Listings />} />
 
         <Route path='/bookings' element={<Bookings />} />
+        
+        <Route path='/admin/user' element={<User />} />
       </Routes>
       <ToastContainer autoClose={500} />
     </BrowserRouter>
