2330c046b75f98932c009d26e482ae4b0379f3ea
Divesh Thapa <dthapa007@gmail.com>
2023-07-05 05:56:19 +0545

    update

--- frontend/src/component/User/ProfileTab.jsx
+++ frontend/src/component/User/ProfileTab.jsx
@@ -2,7 +2,7 @@ import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 
 
-function ProfileTab() {
+function ProfileTab({user}) {
     return (
         <Tabs
             defaultActiveKey="home"
@@ -13,19 +13,19 @@ function ProfileTab() {
             <Tab eventKey="home" title="Details" id="p-details">
                 <div className="text-con d-flex justify-content-between">
                     <h6 className='left-text'>First Name: </h6>
-                    <h6 class="right-text">Chirag</h6>
+                    <h6 class="right-text">{user.fname}</h6>
                 </div>
                 <div className="text-con d-flex justify-content-between">
                     <h6 className='left-text'>Last Name: </h6>
-                    <h6 class="right-text">Simkhada</h6>
+                    <h6 class="right-text">{user.lname}</h6>
                 </div>
                 <div className="text-con d-flex justify-content-between">
                     <h6 className='left-text'>Username: </h6>
-                    <h6 class="right-text">Chirag</h6>
+                    <h6 class="right-text">{user.username}</h6>
                 </div>
                 <div className="text-con d-flex justify-content-between">
                     <h6 className='left-text'>Email: </h6>
-                    <h6 class="right-text">chiragsimkhada@gmail.com</h6>
+                    <h6 class="right-text">{user.email}</h6>
                 </div>
             </Tab>
 
